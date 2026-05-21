b971429eabf26597e1547f00f527e9d965fec560
Flaminia Cavallo <flaminia@dhis2.org>
2026-05-21 10:04:46 +0200

    feat: change contracts generation

--- src/contracts/generateContracts.spec.ts
+++ src/contracts/generateContracts.spec.ts
@@ -8,13 +8,11 @@ const generateContract = <T extends ZodRawShape>({
     path,
     name,
     expectedSchema,
-    usage,
 }: {
     method: string
     path: string
     name: string
     expectedSchema: ZodObject<T>
-    usage?: string
 }) => {
     const contractPath = `contracts/${name}/contract.json`
     const schemaPath = `contracts/${name}/json-schema.json`
@@ -37,12 +35,6 @@ const generateContract = <T extends ZodRawShape>({
         schemaPath,
         JSON.stringify(schema.definitions?.[name], null, 2)
     )
-    if (usage) {
-        writeFileSync(
-            `contracts/${name}/README.md`,
-            `# ${method} ${name} usage\n\n${usage}`
-        )
-    }
 }
 
 describe('contracts', () => {
@@ -52,7 +44,6 @@ describe('contracts', () => {
             path: '/categories/{id}',
             name: 'category',
             expectedSchema: categoryFormSchema,
-            usage: 'used to populate edit category form',
         })
     })
 })
