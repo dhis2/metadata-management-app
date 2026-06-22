0bfb8328fd92b6381a6efc0e4d943f6f4280f6ae
chisom chima <chisomchima370@gmail.com>
2026-06-22 08:24:01 +0100

    feat: enhance DateTimeFieldFF with improved time handling and error management

--- src/components/fields/date/DateTimeFieldFF.tsx
+++ src/components/fields/date/DateTimeFieldFF.tsx
@@ -1,60 +1,83 @@
-import React, { useMemo } from 'react'
+import { Field } from '@dhis2/ui'
+import React, { useCallback, useMemo } from 'react'
 import { DateFieldFF, DateFieldProps } from './DateFieldFF'
 import css from './DateTimeField.module.css'
 import { TimeInput } from './TimeInput'
 
-const defaultTime = '12:00'
+const DEFAULT_TIME = '12:00'
 
-export const DateTimeFieldFF = (props: DateFieldProps) => {
-    const originalInput = props.input
+export const DateTimeFieldFF = ({
+    input: originalInput,
+    meta,
+    label,
+}: DateFieldProps) => {
     const [datePart, timePart] = (originalInput.value ?? '').split('T')
 
-    const input = useMemo(() => {
-        const handleChange = (calendarDateString: string, newTime?: string) => {
-            if (!calendarDateString) {
-                if (newTime) {
-                    originalInput.onChange('T' + newTime)
-                } else if (newTime === '') {
-                    originalInput.onChange('')
-                    originalInput.onBlur()
-                } else {
-                    originalInput.onChange(timePart ? `T${timePart}` : '')
-                    originalInput.onBlur()
-                }
-                return
-            }
-            const resolvedTime = newTime ?? timePart ?? defaultTime
-            const dateTime = `${calendarDateString}T${resolvedTime}`
-            originalInput.onChange(dateTime)
+    const handleDateChange = useCallback(
+        (calendarDateString: string) => {
+            const newValue = calendarDateString
+                ? `${calendarDateString}T${timePart ?? DEFAULT_TIME}`
+                : timePart
+                ? `T${timePart}`
+                : ''
+            originalInput.onChange(newValue)
             originalInput.onBlur()
-        }
-        return {
+        },
+        [originalInput, timePart]
+    )
+
+    const handleTimeChange = useCallback(
+        (newTime: string) => {
+            const newValue = datePart
+                ? `${datePart}T${newTime}`
+                : newTime
+                ? `T${newTime}`
+                : ''
+            originalInput.onChange(newValue)
+            originalInput.onBlur()
+        },
+        [originalInput, datePart]
+    )
+
+    const dateInput = useMemo(
+        () => ({
             ...originalInput,
             value: datePart,
-            onChange: handleChange,
-        }
-    }, [timePart, originalInput, datePart])
+            onChange: handleDateChange,
+        }),
+        [originalInput, datePart, handleDateChange]
+    )
+
+    const formError = meta?.touched ? meta?.error : undefined
+    const timeError = datePart && !timePart ? formError : undefined
+    const dateError = timeError ? undefined : formError
 
     return (
         <div className={css.wrapper}>
             <DateFieldFF
                 className={css.dateField}
                 inputWidth="100%"
-                meta={props.meta}
-                input={input}
-                label={props.label}
+                meta={{ ...meta, error: dateError }}
+                input={dateInput}
+                label={label}
                 valid={false}
             />
-            <TimeInput
+            <Field
                 className={css.timeInput}
-                width="100%"
-                value={timePart || ''}
-                onChange={(payload) => {
-                    input.onChange(datePart, payload.value)
-                }}
-                onBlur={() => input.onBlur()}
-                clearable
-            />
+                error={!!timeError}
+                validationText={timeError}
+            >
+                <TimeInput
+                    width="100%"
+                    value={timePart || ''}
+                    onChange={(payload) =>
+                        handleTimeChange(payload.value ?? '')
+                    }
+                    onBlur={() => originalInput.onBlur()}
+                    clearable
+                    error={!!timeError}
+                />
+            </Field>
         </div>
     )
 }
