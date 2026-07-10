b12f00e66b1d951afce1f4e5c909d8913f93071f
Thomas Zemp <tom@dhis2.org>
2026-07-10 16:02:00 +0200

    feat: add categories specific validation

--- src/pages/categories/Merge.tsx
+++ src/pages/categories/Merge.tsx
@@ -1,6 +1,7 @@
 import { useDataEngine } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
-import React, { useMemo } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import React, { useCallback, useMemo, useState } from 'react'
 import { Form } from 'react-final-form'
 import {
     DefaultMergeFormContents,
@@ -8,7 +9,11 @@ import {
     StyledMergeForm,
     Title,
 } from '../../components/merge'
-import { getDefaultsOld, useLocationWithState } from '../../lib'
+import {
+    getDefaultsOld,
+    useLocationWithState,
+    useBoundResourceQueryFn,
+} from '../../lib'
 import { createFormError } from '../../lib/form/createFormError'
 import { CategoryMergeFormFields } from './merge/CategoryMergeFormFields'
 import {
@@ -17,7 +22,45 @@ import {
     validate,
 } from './merge/CategoryMergeSchema'
 
+type CategoryDetails = {
+    id: string
+    categoryCombo: { id: string }
+    categoryOptions: { id: string }[]
+}
+type CategoriesDetailsResponse = { categories: CategoryDetails[] }
+
+export const useCategoriesDetailsQuery = ({
+    selectedIdsString,
+}: {
+    selectedIdsString: string
+}) => {
+    const queryFn = useBoundResourceQueryFn()
+
+    return useQuery({
+        queryKey: [
+            {
+                resource: 'categories',
+                params: {
+                    fields: ['id', 'categoryCombo[id]', 'categoryOptions[id]'],
+                    filter: [`id:in:[${selectedIdsString}]`],
+                    paging: false,
+                },
+            },
+        ],
+        enabled: !!selectedIdsString,
+        queryFn: queryFn<CategoriesDetailsResponse>,
+        select: useCallback((data: CategoriesDetailsResponse) => {
+            return data.categories.reduce((acc, category) => {
+                acc[category.id] = category
+                return acc
+            }, {} as Record<string, CategoryDetails>)
+        }, []),
+    })
+}
+
 export const Component = () => {
+    const [extraValidationResult, setExtraValidationResult] =
+        useState<boolean>(true)
     const location = useLocationWithState<{ selectedModels: Set<string> }>()
 
     const dataEngine = useDataEngine()
@@ -32,6 +75,56 @@ export const Component = () => {
             sources: [],
         }),
         []
+    )
+
+    const { data: categoryDetails } = useCategoriesDetailsQuery({
+        selectedIdsString: selectedIds.join(','),
+    })
+
+    const extraValidate: (values: {
+        target: string
+        sources: string[]
+    }) => string | undefined = useCallback(
+        (values) => {
+            if (!values.target || !values.sources || !categoryDetails) {
+                return undefined
+            }
+            // keep target as poin of reference
+            const targetCategory = categoryDetails[values.target]
+            const targetCategoryOptions = targetCategory.categoryOptions
+                .map((co) => co.id)
+                .sort()
+                .join(',')
+            for (const category of values.sources) {
+                const sourceCategory = categoryDetails[category]
+                const sourceCategoryOptions = sourceCategory.categoryOptions
+                    .map((co) => co.id)
+                    .sort()
+                    .join(',')
+
+                // if category options are not the same
+                if (sourceCategoryOptions !== targetCategoryOptions) {
+                    setExtraValidationResult(false)
+                    return i18n.t(
+                        'Category options of source and target categories do not match'
+                    )
+                }
+                // if category combos are not the same
+                if (
+                    sourceCategory.categoryCombo.id !==
+                    targetCategory.categoryCombo.id
+                ) {
+                    setExtraValidationResult(false)
+                    return i18n.t(
+                        'Category combos of source and target categories do not match'
+                    )
+                }
+            }
+            // if category combo of any items do not match
+            setExtraValidationResult(true)
+            return undefined
+        },
+        [categoryDetails, setExtraValidationResult]
     )
 
     const onSubmit = async (values: CategoryMergeFormValues) => {
@@ -80,8 +173,12 @@ export const Component = () => {
                                 </p>
                             </MergeComplete>
                         }
+                        extraValidation={extraValidate}
                     >
-                        <CategoryMergeFormFields selectedIds={selectedIds} />
+                        <CategoryMergeFormFields
+                            selectedIds={selectedIds}
+                            hideConfirmation={!extraValidationResult}
+                        />
                     </DefaultMergeFormContents>
                 </StyledMergeForm>
             )}
