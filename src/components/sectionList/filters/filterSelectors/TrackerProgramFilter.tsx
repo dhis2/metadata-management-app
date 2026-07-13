3ac564c48f08f3c655e679055f660bf3bae9980c
Flaminia Cavallo <flaminia@dhis2.org>
2026-07-13 13:20:46 +0200

    refactor: remove unused import

--- src/components/sectionList/filters/filterSelectors/TrackerProgramFilter.tsx
+++ src/components/sectionList/filters/filterSelectors/TrackerProgramFilter.tsx
@@ -1,7 +1,6 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import { useSectionListFilter } from '../../../../lib'
-import { createFilterDataQuery } from './createFilterDataQuery'
 import { ModelFilterSelect } from './ModelFilter'
 
 const query = {
