b8fd0085b637a026265dcec3d72f8d5250560e16
henrikmv <110386561+henrikmv@users.noreply.github.com>
2026-07-23 07:39:46 +0000

    fix: format

--- src/lib/constants/translatedModelProperties.ts
+++ src/lib/constants/translatedModelProperties.ts
@@ -104,7 +104,9 @@ const TRANSLATED_PROPERTY: Record<string, string> = {
     trackedEntityTypesLabel: i18n.t('Name (Plural)'),
     notesLabel: i18n.t('Note label (Plural)'),
     relationshipsLabel: i18n.t('Relationship label (Plural)'),
-    trackedEntityAttributesLabel: i18n.t('Tracked entity attribute label (Plural)'),
+    trackedEntityAttributesLabel: i18n.t(
+        'Tracked entity attribute label (Plural)'
+    ),
 }
 
 const camelCaseToSentenceCase = (camelCase: string) =>
