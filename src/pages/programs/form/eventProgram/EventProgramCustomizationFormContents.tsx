e9f4f1f0e866bab5f5a255f9ac1999669323eee2
henrikmv <110386561+henrikmv@users.noreply.github.com>
2026-09-07 11:14:58 +0000

    feat: add custom labels for event program

--- src/pages/programs/form/eventProgram/EventProgramCustomizationFormContents.tsx
+++ src/pages/programs/form/eventProgram/EventProgramCustomizationFormContents.tsx
@@ -9,11 +9,14 @@ import {
     StandardFormSectionTitle,
 } from '../../../../components'
 import {
+    FEATURES,
     SCHEMA_SECTIONS,
     Section,
+    useFeatureAvailable,
     useGivenShemaOrSchemaSectionHandleOrThrow,
     useValidator,
 } from '../../../../lib'
+import { LabelField } from '../trackerProgram/fields'
 
 export const EventProgramCustomizationFormContents = React.memo(
     function EventProgramCustomizationFormContents({
@@ -24,6 +27,9 @@ export const EventProgramCustomizationFormContents = React.memo(
         section?: Section
     }) {
         useGivenShemaOrSchemaSectionHandleOrThrow({ section })
+        const showPluralLabels = useFeatureAvailable(
+            FEATURES.customTerminologyPlurals
+        )
         const reportDateLabelValidator = useValidator({
             schemaSection: SCHEMA_SECTIONS.programStage,
             property: 'executionDateLabel',
@@ -53,6 +59,67 @@ export const EventProgramCustomizationFormContents = React.memo(
                         validate={reportDateLabelValidator}
                     />
                 </StandardFormField>
+
+                <LabelField
+                    name="eventLabel"
+                    label={i18n.t('Custom label for "event" (singular)')}
+                    helpText={i18n.t('Example use: Schedule a new event', {
+                        nsSeparator: '~:~',
+                    })}
+                />
+                {showPluralLabels && (
+                    <LabelField
+                        name="eventsLabel"
+                        label={i18n.t('Custom label for "events" (plural)')}
+                        helpText={i18n.t('Example use: See all events', {
+                            nsSeparator: '~:~',
+                        })}
+                    />
+                )}
+
+                <LabelField
+                    name="orgUnitLabel"
+                    label={i18n.t('Custom label for "organisation unit"')}
+                    helpText={i18n.t(
+                        'Used to customize the label for the organisation unit that registers the enrollment or event'
+                    )}
+                />
+
+                <LabelField
+                    name="relationshipLabel"
+                    label={i18n.t('Custom label for "relationship" (singular)')}
+                    helpText={i18n.t(
+                        'Used to customize the label for a relationship between tracked entities'
+                    )}
+                />
+                {showPluralLabels && (
+                    <LabelField
+                        name="relationshipsLabel"
+                        label={i18n.t(
+                            'Custom label for "relationships" (plural)'
+                        )}
+                        helpText={i18n.t('Example use: See all relationships', {
+                            nsSeparator: '~:~',
+                        })}
+                    />
+                )}
+
+                <LabelField
+                    name="noteLabel"
+                    label={i18n.t('Custom label for "note" (singular)')}
+                    helpText={i18n.t(
+                        'Used to customize the label for a note or comment added to an enrollment or event'
+                    )}
+                />
+                {showPluralLabels && (
+                    <LabelField
+                        name="notesLabel"
+                        label={i18n.t('Custom label for "notes" (plural)')}
+                        helpText={i18n.t('Example use: See all notes', {
+                            nsSeparator: '~:~',
+                        })}
+                    />
+                )}
             </SectionedFormSection>
         )
     }
